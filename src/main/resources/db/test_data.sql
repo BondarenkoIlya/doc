a95b60ad30ae3494798fdd4b7e217d33068f7a52
BondarenkoIlya <bondarenko_elijah@mail.ru>
2017-01-25 11:38:36 +0600

    #8 Add test database data

---
+++ src/main/resources/db/test_data.sql
@@ -0,0 +1,8 @@
+INSERT INTO roles VALUES (null,'ROLE_ADMIN'),(null,'ROLE_USER');
+
+INSERT INTO users VALUES (null,'Ilya','qwe',1),(null,'Vasya','qwe',1),(null,'Petya','qwe',1);
+
+INSERT INTO user_roles VALUES (null,1,1),(null,1,2),(null,2,2),(null,3,2);
+
+INSERT INTO user_info VALUES (null,'Bondarenko Ilya',1),(null,'Pupkin Vasya',2),(null,'Kalashnikov Petya',3);
+
